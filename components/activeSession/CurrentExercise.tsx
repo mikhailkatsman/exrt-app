f696427c6c8e6e5346fda32eac631c41db29da6a
Mikhail Katsman <kry3er@gmail.com>
2024-06-13 20:05:06 +0100

    step by step smooth reveal

--- components/activeSession/CurrentExercise.tsx
+++ components/activeSession/CurrentExercise.tsx
@@ -1,9 +1,8 @@
 import { exerciseBackgrounds, videoFiles } from "@modules/AssetPaths"
 import { Icon } from "@react-native-material/core"
-import { LinearGradient } from "expo-linear-gradient"
 import { ImageBackground, Text, View, TouchableOpacity, ScrollView } from "react-native"
-import Animated, { Easing, useSharedValue, withTiming, withDelay, useAnimatedStyle } from "react-native-reanimated"
-import { useEffect } from "react"
+import Animated, { Easing, useSharedValue, withTiming, withDelay, useAnimatedScrollHandler, useAnimatedStyle } from "react-native-reanimated"
+import { useEffect, useState } from "react"
 import { useNavigation } from "@react-navigation/native"
 import ActivityTimer from "./ActivityTimer"
 
@@ -34,6 +33,13 @@ const CurrentExercise: React.FC<Props> = ({
 	showTimer,
 	setShowTimer
 }) => {
+	const [parentHeight, setParentHeight] = useState(0);
+	const topViewHeightPercentage = 75;
+	const topViewMinHeightPercentage = 25;
+
+	const topViewHeight = useSharedValue(0)
+	const scrollY = useSharedValue(0)
+
 	const opacity = useSharedValue(0)
 	const translateX = useSharedValue(screenWidth)
 
@@ -52,6 +58,27 @@ const CurrentExercise: React.FC<Props> = ({
 		}
 	})
 
+	const topViewAnimatedStyle = useAnimatedStyle(() => {
+    const minHeight = parentHeight * (topViewMinHeightPercentage / 100);
+    const height = Math.max(
+      minHeight,
+      parentHeight * (topViewHeightPercentage / 100) - scrollY.value
+    );
+    return { height: withTiming(height, { duration: 50, easing: Easing.linear }) };
+  });
+
+	const handleScroll = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      scrollY.value = event.contentOffset.y;
+    },
+  });
+
+	const handleParentLayout = (event: any) => {
+    const { height } = event.nativeEvent.layout;
+    setParentHeight(height);
+    topViewHeight.value = height * (topViewHeightPercentage / 100);
+  };
+
 	return (
 		<Animated.View style={animatedStyle} className="w-full h-full">
 			{showTimer ? (
@@ -63,64 +90,66 @@ const CurrentExercise: React.FC<Props> = ({
 					}}
 				/>
 			) : (
-				<View className="h-full w-full rounded-2xl overflow-hidden">
-					<ImageBackground
-						className="flex-1"
-						resizeMode="cover"
-						source={exerciseBackgrounds[background] || { uri: background }} 
+				<View className="flex-1" onLayout={handleParentLayout}>
+					<Animated.View 
+						className="rounded-2xl overflow-hidden"
+						style={topViewAnimatedStyle}
 					>
-						<View className="flex-1">
-							<View className="px-4 pt-4 flex-row justify-between">
-								<View className="flex-1">
-									<Text className="text-custom-white font-BaiJamjuree-Bold text-3xl uppercase">
-										{name}
-									</Text>
-									<Text className="text-custom-white font-BaiJamjuree-RegularItalic text-lg capitalize">
-										{style}
-									</Text>
-								</View>
-								<View>
-									<TouchableOpacity
-										className="mb-3"
-										onPress={() => navigation.navigate('FullScreenVideo', { videoSource: video })}
-									>
-										<Icon name="video-outline" size={40} color="#F5F6F3" />
-									</TouchableOpacity>
-									<TouchableOpacity
-										onPress={() => navigation.navigate('ExerciseDetails', {
-												exerciseId: exerciseId,
-												fromActiveSession: true
-										})}
-									>
-										<Icon name="information-outline" size={40} color="#F5F6F3" />
-									</TouchableOpacity>
-								</View>
-							</View>
-							<ScrollView
-								className="z-10 absolute top-40 w-full bottom-0"
-								showsVerticalScrollIndicator={false}
-								bounces={false}
-								alwaysBounceVertical={false}
-								fadingEdgeLength={100}
-							>
-								<View className="h-28 w-full" />
-								<LinearGradient 
-									className="h-36 w-full"
-									colors={['transparent', 'transparent', '#121212']}
-								/>
-								<View className='px-4 pb-3 bg-custom-dark w-full'>
-									<Text className="text-custom-white font-BaiJamjuree-MediumItalic">Step By Step:</Text>
-								</View>
-								{execution.map((paragraph, index) => (
-									<View key={index} className="w-full flex flex-row pb-3 px-4 bg-custom-dark border">
-										<Text className="w-[10%] pt-0.5 text-custom-red font-BaiJamjuree-Bold text-lg">{index + 1}</Text>
-										<Text className="w-[80%] text-custom-white font-BaiJamjuree-Regular">{paragraph}</Text>
+						<ImageBackground
+							className="flex-1"
+							resizeMode="cover"
+							source={exerciseBackgrounds[background] || { uri: background }} 
+						>
+							<View className="flex-1">
+								<View className="px-4 pt-4 flex-row justify-between">
+									<View className="flex-1">
+										<Text className="text-custom-white font-BaiJamjuree-Bold text-3xl capitalize">
+											{name}
+										</Text>
+										<Text className="text-custom-white font-BaiJamjuree-RegularItalic text-lg capitalize">
+											{style}
+										</Text>
 									</View>
-								))}
-							</ScrollView>
-							<View className="absolute w-full bottom-0 bg-custom-dark h-20 z-0" />
+									<View>
+										<TouchableOpacity
+											className="mb-3"
+											onPress={() => navigation.navigate('FullScreenVideo', { videoSource: video })}
+										>
+											<Icon name="video-outline" size={40} color="#F5F6F3" />
+										</TouchableOpacity>
+										<TouchableOpacity
+											onPress={() => navigation.navigate('ExerciseDetails', {
+													exerciseId: exerciseId,
+													fromActiveSession: true
+											})}
+										>
+											<Icon name="information-outline" size={40} color="#F5F6F3" />
+										</TouchableOpacity>
+									</View>
+								</View>
+								
+							</View>
+						</ImageBackground>
+					</Animated.View>
+					<Animated.ScrollView
+						className="flex-1"
+						showsVerticalScrollIndicator={false}
+						bounces={false}
+						alwaysBounceVertical={false}
+						fadingEdgeLength={100}
+						onScroll={handleScroll}
+						scrollEventThrottle={16}
+					>
+						<View className='p-4'>
+							<Text className="text-custom-white font-BaiJamjuree-MediumItalic">Step By Step:</Text>
 						</View>
-					</ImageBackground>
+						{execution.map((paragraph, index) => (
+							<View key={index} className="flex flex-row pb-3 px-4">
+								<Text className="w-[10%] pt-0.5 text-custom-red font-BaiJamjuree-Bold text-lg">{index + 1}</Text>
+								<Text className="w-[80%] text-custom-white font-BaiJamjuree-Regular">{paragraph}</Text>
+							</View>
+						))}
+					</Animated.ScrollView>
 				</View>
 			)}
 		</Animated.View>
